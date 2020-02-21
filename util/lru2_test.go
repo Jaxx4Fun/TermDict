9e176a2a6d4c0c2bf492b77c6f054d9cccf0a0b9
johnny4fun <309329794@qq.com>
2020-02-22 03:21:34 +0800

    use lru2

--- util/lru2_test.go
+++ util/lru2_test.go
@@ -1,21 +1,15 @@
 package util_test
 
 import (
+	"github.com/johnny4fun/TermDict/util"
 	"strconv"
+	"strings"
 	"testing"
 )
 
-type TestObject struct {
-	key string
-}
-
-func (o *TestObject) Key() string {
-	return o.key
-}
-
 func TestLRU(t *testing.T) {
 	t.Run("empty LRU pop", func(t *testing.T) {
-		lru := util.NewLRUCache(10)
+		lru := util.NewLRUCache2(10)
 
 		obj := lru.PopFront()
 
@@ -23,7 +17,7 @@ func TestLRU(t *testing.T) {
 	})
 
 	t.Run("LRU add", func(t *testing.T) {
-		lru := util.NewLRUCache(10)
+		lru := util.NewLRUCache2(10)
 
 		for i := 0; i < 10; i++ {
 			obj := &TestObject{strconv.Itoa(i)}
@@ -32,9 +26,9 @@ func TestLRU(t *testing.T) {
 
 		assertLRUSize(t, lru, 10)
 
-		storage := lru.GetStorageMap()
+		want := strings.Split("0,1,2,3,4,5,6,7,8,9",",")
 		for i := 0; i < 10; i++ {
-			assertMapContains(t, storage, strconv.Itoa(i))
+			assertLRUContent(t, lru, want)
 		}
 
 		t.Run("LRU add more the capacity limit", func(t *testing.T) {
@@ -45,16 +39,16 @@ func TestLRU(t *testing.T) {
 
 			assertLRUSize(t, lru, 10)
 			for i := 0; i < 10; i++ {
-				assertMapNotContains(t, storage, strconv.Itoa(i))
+				assertLRUNotContains(t, lru, strconv.Itoa(i))
 			}
 			for i := 10; i < 20; i++ {
-				assertMapContains(t, storage, strconv.Itoa(i))
+				assertLRUContains(t, lru, strconv.Itoa(i))
 			}
 		})
 	})
 
 	t.Run("query when lru is not empty", func(t *testing.T) {
-		lru := util.NewLRUCache(10)
+		lru := util.NewLRUCache2(10)
 		for i := 0; i < 10; i++ {
 			obj := &TestObject{strconv.Itoa(i)}
 			lru.Add(obj)
@@ -65,26 +59,20 @@ func TestLRU(t *testing.T) {
 		// query the oldest one
 		lru.Get("0")
 		lru.Get("1")
+		var want []string
+		want = strings.Split("2,3,4,5,6,7,8,9,0,1", ",")
+		assertLRUContent(t, lru, want)
 
 		// query item in mid
 		lru.Get("9")
+		want = strings.Split("2,3,4,5,6,7,8,0,1,9", ",")
+		assertLRUContent(t, lru, want)
 
 		// query last item
 		lru.Get("9")
 
-		want := "2345678019"
-		// pop all
-		got := [10]byte{}
-		for i := 0; i < 10; i++ {
-			obj := lru.PopFront()
-			got[i] = obj.Key()[0]
-		}
-
-		if string(got[:]) != want {
-			t.Errorf("got %v, want %s", got, want)
-		}
-
-
+		want = strings.Split("2,3,4,5,6,7,8,0,1,9",",")
+		assertLRUContent(t, lru, want)
 	})
 
 	t.Run("clear lru cache", func(t *testing.T) {
@@ -103,6 +91,23 @@ func TestLRU(t *testing.T) {
 	})
 }
 
+func assertLRUContent(t *testing.T, lru *util.LRUCache2, want []string) {
+	t.Helper()
+
+	objs := lru.GetContentByOrder()
+
+	if got, want := len(objs), len(want); got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+
+	for i := uint(0); i < lru.Size(); i++ {
+		if got, want := objs[i].Key(), want[i]; got != want {
+			t.Errorf("item[%d]: got %q, want %q", i, got, want)
+		}
+	}
+
+}
+
 func assertObject(t *testing.T, obj util.Object, expect util.Object) {
 	t.Helper()
 	if obj != expect {
@@ -110,21 +115,33 @@ func assertObject(t *testing.T, obj util.Object, expect util.Object) {
 	}
 }
 
-func assertMapContains(t *testing.T, storageMap map[string]*util.Node, want string) {
+func assertLRUContains(t *testing.T, lru util.LRU, want string) {
 	t.Helper()
-	if _, ok := storageMap[want]; !ok {
-		t.Errorf("key %q expected in %v", want, storageMap)
+
+	objs := lru.GetContentByOrder()
+
+	for _, obj := range objs {
+		if obj.Key() == want {
+			return
+		}
+	}
+
+	t.Errorf("%q not expected in %v", want, objs)
+}
+
+func assertLRUNotContains(t *testing.T, lru util.LRU, want string) {
+	t.Helper()
+
+	objs := lru.GetContentByOrder()
+
+	for _, obj := range objs {
+		if obj.Key() == want {
+			t.Fatalf("%q not expected in %v", want, objs)
+		}
 	}
 }
 
-func assertMapNotContains(t *testing.T, storageMap map[string]*util.Node, want string) {
-	t.Helper()
-	if _, ok := storageMap[want]; ok {
-		t.Errorf("key %q expected NOT in %v", want, storageMap)
-	}
-}
-
-func assertLRUSize(t *testing.T, lru *util.LRUCache, want uint) {
+func assertLRUSize(t *testing.T, lru util.LRU, want uint) {
 	t.Helper()
 	if got := lru.Size(); got != want {
 		t.Errorf("LRU size is %d, expect %d", got, want)
---
+++ util/node.go
@@ -0,0 +1,17 @@
+package util
+
+type Object interface {
+	Key() string
+}
+
+type Node struct {
+	Prev *Node
+	Next *Node
+	data Object
+}
+
+func NewNode(obj Object) *Node{
+	return &Node{
+		data:obj,
+	}
+}
